db4b611f38f1d6d8ec9fc409ddf004893c5b0e4d
Ric <rdcalle@gmail.com>
2016-10-02 03:40:29 +0200

    Finished the new screenlog.

--- dec-update.js
+++ dec-update.js
@@ -5,17 +5,13 @@
 const icons       = "picon.tar.bz2",
       origDefault = "192.168.1.204:22",
       destDefault = "192.168.1.222:22";
-const DOWN  = ['DOWN', 'red'],
-      NO    = ['NO', 'red'],
-      UP    = ['UP', 'green'],
-      YES   = ['YES', 'green'];
-
+      noCkech     = '-oStrictHostKeyChecking=no'; // to avoid check different host dynIP
 
 // import required libraries
 const exec = require('child_process').exec;
 const argv = require('yargs')
-      .usage('Uso: $0 --orig [ip]:[puerto] --dest [ip]:[puerto]')
-      .default({'orig': origDefault, 'dest': destDefault})
+      .usage('Uso: $0 --orig [ip]:[puerto] --dest [ip]:[puerto] --port [puerto]')
+      .default({'orig': origDefault, 'port': '22'})
       .demand(['orig', 'dest'])
       .showHelpOnFail(true)
       .argv;
@@ -26,8 +22,17 @@ const clear       = require('clear')
       CLI         = require('clui'),
       clc         = require('cli-color'),
       Line        = CLI.Line,
-      LineBuffer  = CLI.LineBuffer;
+      LineBuffer  = CLI.LineBuffer,
+      Spinner     = CLI.Spinner;
 
+const DOWN  = ['DOWN', 'red'],
+      NO    = ['NO', 'red'],
+      UP    = ['UP', 'bgGreen'],
+      YES   = ['YES', 'bgGreen'];
+
+// To indicate a message about what it is doing
+var spin = new Spinner('');
+spin.start();
 
 //// Set an array with the destination target
 function setDest(dest) {
@@ -39,7 +44,7 @@ function setDest(dest) {
         const [ ip, port ] = host.split(':');
         return {
           host: ip,
-          port: port || '22',
+          port: port || argv.port,
           state: DOWN,
           channels: NO,
           iconsUp: NO,
@@ -51,14 +56,11 @@ function setDest(dest) {
 }
 
 //// Execute a shell linux command and return its result wrapped in a Promise
-function command(sentence, msg) {
+function command(sentence) {
   return new Promise(function(resolve, reject) {
     try {
       exec(sentence, {maxBuffer: 1024 * 500}, function(err, stdout, stderr) {
-        if (err) {
-          reject(err)
-        }
-        term.cyan(msg || stdout);
+        if (err) reject(err)
         resolve();
       });
     } catch(e) {
@@ -73,56 +75,6 @@ function commands(list) {
     prev.push(command(curr[0], curr[1]));
     return prev;
   }, []));
-}
-
-//// linux commands to be executed
-async function execute() {
-  const origTimeLbl = '    Tiempo empleado';
-  const noCkech = '-oStrictHostKeyChecking=no'; // to avoid check different host dynIP
-  try {
-    // First, we get channel list and picons from the origin decoder
-    console.time(origTimeLbl);
-    term.bold(`\n -- Obteniendo los archivos necesarios de `).bgBlue(`${argv.orig}:${pOrig}\n`);
-    await commands([
-      [
-        `ssh ${noCkech} -p ${pOrig} root@${argv.orig} "tar cf - /etc/enigma2" | tar xvf -`,
-        `  - Obtenida configuración de la lista de canales -> ./etc/enigma2\n`
-      ]
-      // ],
-      // [
-      //   `ssh ${noCkech} -p ${pOrig} root@${argv.orig} "cd /hdd && tar cf - picon" | bzip2 - > ${icons}`,
-      //   `  - Obtenidos iconos de los canales -> ./${icons}\n`
-      // ]
-    ]);
-    console.timeEnd(origTimeLbl);
-
-    // Then, we send them to the destination decoder
-    // term.bold(`\n -- Subiendo los archivos a `).bgMagenta(`${argv.dest}:${pDest}`)(` (canales e iconos)\n`);
-    // await commands([
-    //   [
-    //     `scp ${noCkech} -P ${pDest} \`find etc/ | egrep 'lamedb|list|bouquet|satellites'\` root@${argv.dest}:/etc/enigma2/`,
-    //     `  - Subida la configuración de los canales\n`
-    //   ],
-    //   [
-    //     `scp ${noCkech} -P ${pDest} -r ./${icons} root@${argv.dest}:`,
-    //     `  - Subidos los iconos actualizados (extrayendo en remoto... esto tardará un pelín)\n`
-    //   ]
-    // ]);
-    // And it extracts picon file
-    // await command(
-    //   `ssh ${noCkech} -p ${pDest} root@${argv.dest} "tar xjf ${icons} -C /usr/share/enigma2/ && rm -rf ${icons}"`,
-    //   `  - Extraídos ya todos los ficheros de los iconos de los canales\n`
-    // );
-    // Finally, we remove the temporal dirs
-    await command(
-      `rm -rf ./etc ./${icons}`,
-      `\n -- Borrados todos ficheros temporales`
-    );
-    term.bgGreen.bold("\n¡Proceso finalizado correctamente!\n");
-  }
-  catch(err) {
-    console.log(`${err}`);
-  }
 }
 
 //// To check a host is available
@@ -144,7 +96,7 @@ function main() {
   const [ hostOrig, pOrig ] = argv.orig.split(':');
   let orig = {
     host:     hostOrig,
-    port:     pOrig || '22',
+    port:     pOrig || argv.port,
     state:    DOWN,
     channels: NO,
     icons:    NO
@@ -154,30 +106,72 @@ function main() {
   // Clears screen and welcome message
   clear();
   term.blue(figlet.textSync('DEC-UPDATE', { horizontalLayout: 'full' }));
-  draw(orig, dest);
+  draw(orig, dest, `Connecting ${orig.host} on port ${orig.port}`);
 
-  checkHost(orig).then(() => {
+  checkHost(orig)
+  .then(() => {
     orig.state = UP;
+    draw(orig, dest, `Retrieving channels from ${orig.host}`);
+    // Getting the channel list from the origin host
+    return command(`ssh ${noCkech} -p ${orig.port} root@${orig.host} "tar cf - /etc/enigma2" | tar xvf -`)
+  })
+  .then(() => {
+    orig.channels = YES;
+    draw(orig, dest, `Getting the icons pack from ${orig.host}`);
+    // Retrieving the icons pack
+    return command(`ssh ${noCkech} -p ${orig.port} root@${orig.host} "cd /hdd && tar cf - picon" | bzip2 - > ${icons}`);
+  })
+  .then(() => {
+    orig.icons = YES;
     draw(orig, dest);
-    dest.map(host => {
-      checkHost(host)
-      .then(() => UP)
-      .catch(() => DOWN)
-      .then((state) => {
-        host.state = state;
-        draw(orig, dest);
-      });
-    });
-  });
-  // .then(() => execute())
-  // .catch((err) => {
-  //   console.log(err);
-  //   return false;
-  // });
+    // Now, we can do the broadcasting to each target
+    return Promise.all(dest.map(target => {
+      return new Promise((resolve, reject) => {
+        checkHost(target)
+        .then((state) => {
+          target.state = UP;
+          draw(orig, dest, `Uploading channels to ${target.host}`);
+          // Uploading channels list
+          return command(
+            `scp ${noCkech} -P ${target.port} \`find etc/ | egrep 'lamedb|list|bouquet|satellites'\` root@${target.host}:/etc/enigma2/`
+          );
+        })
+        .then(() => {
+          target.channels = YES;
+          draw(orig, dest, `Uploading icons pack to ${target.host}`);
+          // Uploading icons
+          return command(`scp ${noCkech} -P ${target.port} -r ./${icons} root@${target.host}:`);
+        })
+        .then(() => {
+          target.iconsUp = YES;
+          draw(orig, dest, `Extracting icons into the ${target.host}`);
+          // Extracting icons
+          return command(
+            `ssh ${noCkech} -p ${target.port} root@${target.host} "tar xjf ${icons} -C /usr/share/enigma2/ && rm -rf ${icons}"`
+          );
+        })
+        .then(() => {
+          target.iconsSet = YES;
+          draw(orig, dest);
+          resolve();
+        })
+        .catch(() => reject());
+      })
+    }));
+  })
+  .catch(() => {
+    spin.message(`Aborted process.`);
+  })
+  .then(() => {
+    spin.stop();
+    term.white(`\n\nErasing temporal files... `);
+    command(`rm -rf ./etc ./${icons}`);
+    term.bold(`Bye!\n\n`);
+  })
 }
 
 //// It draws on the screen
-function draw(orig, dest) {
+function draw(orig, dest, spinMsg='') {
   const outputBuffer = new LineBuffer({
     x: 0,
     y: 7,
@@ -190,7 +184,8 @@ function draw(orig, dest) {
 
   new Line(outputBuffer)
     .padding(1)
-    .column('Origin Host', 30, [clc.magenta])
+    .column('Origin Host', 26, [clc.magenta])
+    .column('Port', 6, [clc.magenta])
     .column('UP/DOWN', 10, [clc.magenta])
     .column('Channel list retrieved', 25, [clc.magenta])
     .column('Icons pack retrieved', 20, [clc.magenta])
@@ -198,9 +193,10 @@ function draw(orig, dest) {
     .store();
   blankLine();
 
-  new Line(outputBuffer)
+  const origin = new Line(outputBuffer)
     .padding(1)
-    .column(`${orig.host}:${orig.port}`, 32)
+    .column(orig.host, 26, [clc.green])
+    .column(orig.port, 7, [clc.green])
     .column(orig.state[0], 16, [clc[orig.state[1]]])
     .column(orig.channels[0], 25, [clc[orig.channels[1]]])
     .column(orig.icons[0], 3, [clc[orig.icons[1]]])
@@ -209,31 +205,37 @@ function draw(orig, dest) {
   blankLine();
   blankLine();
 
-  new Line(outputBuffer)
+  const { state, channels, icons } = orig;
+  if (state===UP && channels===YES && icons===YES) {
+    new Line(outputBuffer)
     .padding(1)
-    .column('Target IP Host', 30, [clc.magenta])
+    .column('Target IP Host', 22, [clc.magenta])
+    .column('Target Port', 16, [clc.magenta])
     .column('UP/DOWN', 10, [clc.magenta])
     .column('Channels configured', 22, [clc.magenta])
     .column('Icons uploaded', 18, [clc.magenta])
     .column('Icons established', 20, [clc.magenta])
     .fill()
     .store();
-  blankLine();
+    blankLine();
 
-  dest.map(target => {
-    new Line(outputBuffer)
+    dest.map(target => {
+      new Line(outputBuffer)
       .padding(1)
-      .column(`${target.host}:${target.port}`, 32)
+      .column(target.host, 26, [clc.blue])
+      .column(target.port, 14, [clc.blue])
       .column(target.state[0], 16, [clc[target.state[1]]])
       .column(target.channels[0], 20, [clc[target.channels[1]]])
       .column(target.iconsUp[0], 18, [clc[target.iconsUp[1]]])
       .column(target.iconsSet[0], 20, [clc[target.iconsSet[1]]])
       .fill()
       .store();
-  })
-  blankLine();
+    })
+    blankLine();
+  }
 
   outputBuffer.output();
+  spin.message(spinMsg);
 }
 
 main();
